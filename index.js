9350ae9cd2e2ec94953cd469069e0b14a80a2ee3
DESKTOP-CTP2HUU\Mitchell <mitchellwu076@gmail.com>
2020-12-21 19:04:00 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+});
+
+app.get("/api/hello-world-get", (req, res) => {
+  res.send('hello world');
+});
+
+app.post("/api/hello-world-post", (req, res) => {
+  res.send(req.body);
+});
